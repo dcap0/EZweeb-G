4e1d11440cc305d4848063079842915896d7126b
Dennis Capone <dennis.d.cap@gmail.com>
2023-06-14 02:05:08 -0400

    turned ui back on

--- cmd/ezweeb/ezweeb.go
+++ cmd/ezweeb/ezweeb.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"github.com/dcap0/EZ-weeb-G/pkg/logic"
+	"github.com/dcap0/EZ-weeb-G/pkg/tui"
 )
 
 // main starts the program.
 // initializes the TUI and provides it the series data
 func main() {
-	// tui.InitUI(logic.GetSeriesHtml())
-	s := logic.GetSeriesHtml()
-
-	logic.GetSeriesDownloadLink(s[0].Title)
+	tui.InitUI(logic.GetSeriesHtml())
 }
