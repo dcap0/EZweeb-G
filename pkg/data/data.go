6b4bc4466eed9f2258df3df4a174950607cfdce9
Dennis Capone <dennis.d.cap@gmail.com>
2023-06-15 00:33:40 -0400

    Rename package and directory

---
+++ pkg/data/data.go
@@ -0,0 +1,13 @@
+// This package provides structs for options data and show data
+// for a series.
+package data
+
+//Holds data for a series
+type Series struct {
+	Title, Description string
+}
+
+//Holds user options
+type Options struct {
+	Quality, SubLang string
+}
